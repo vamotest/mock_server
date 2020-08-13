3822fdcd9370211845c697c9085789bd0eb9feef
vamotest <vamotest@gmail.com>
2020-08-13 21:49:03 +0300

    Add fixtures

---
+++ conftest.py
@@ -0,0 +1,97 @@
+from utils.generate_test_data import GenerateTestData
+
+import random
+import uuid
+import pytest
+
+fake = GenerateTestData().fake
+
+
+@pytest.fixture()
+def create_client_url():
+    """
+    Фикстура отдает URL для создания нового клиента.
+    """
+    return 'http://mock-server:5000/service/v1/client/create'
+
+
+@pytest.fixture()
+def create_order_url():
+    """
+    Фикстура отдает URL для создания нового заказа.
+    """
+    return 'http://mock-server:5000/service/v1/order/create'
+
+
+@pytest.fixture()
+def create_purchase_client_items_url():
+    """
+    Фикстура отдает URL для проверки сколько раз клиент покупал/не покупал
+    конкретные `item_id` (из запроса), номер последнего заказа
+    и дату его создания.
+    """
+    return 'http://mock-server:5000/service/v1/item/purchase/by-client'
+
+
+@pytest.fixture()
+def user_name():
+    """
+    Фикстура отдает корректное имя клиента.
+    """
+    return fake.first_name()
+
+
+@pytest.fixture()
+def user_surname():
+    """
+    Фикстура отдает корректную фамилию клиента.
+    """
+    return fake.last_name()
+
+
+@pytest.fixture()
+def user_phone():
+    """
+    Фикстура отдает корректный номер клиента.
+    """
+    return fake.phone_number()
+
+
+@pytest.fixture()
+def user_address():
+    """
+    Фикстура отдает корректный адрес клиента.
+    """
+    return fake.address()
+
+
+@pytest.fixture()
+def order_item_id():
+    """
+    Фикстура отдает случайное значение `item_id` в заданном диапозоне.
+    """
+    return random.randint(10000, 99999)
+
+
+@pytest.fixture()
+def order_price():
+    """
+    Фикстура отдает случайное значение `price` в заданном диапозоне.
+    """
+    return float(random.randint(100, 1000))
+
+
+@pytest.fixture()
+def order_quantity():
+    """
+    Фикстура отдает случайное значение количества товара в заданном диапозоне.
+    """
+    return random.randint(1, 20)
+
+
+@pytest.fixture()
+def user_client_id():
+    """
+    Фикстура отдает случайной `client_id`.
+    """
+    return uuid.uuid4().int
