35c664cbdb8e4b8fc83969b1afeffcf0bb09e484
vamotest <vamotest@gmail.com>
2020-08-14 16:38:28 +0300

    Add different length item_id

--- tests/test_create_order_item_id.py
+++ tests/test_create_order_item_id.py
@@ -1,6 +1,8 @@
 from utils.generate_test_data import GenerateTestData
 from utils.validate_server_response import ServerResponse
 
+import random
+
 import pytest
 import requests
 
@@ -19,7 +21,7 @@ class TestOrderItemId:
 			self, param_order_item_id, create_order_url, user_client_id,
 			user_address, user_phone, order_price, order_quantity):
 		"""
-		Проверка позитивных сценариев для введеного количества `item_id`
+		Проверка позитивных сценариев для введенного количества `item_id`
 		при создании заказа.
 		"""
 
@@ -48,12 +50,51 @@ class TestOrderItemId:
 		server_response.validate_create_order(response)
 
 	@pytest.mark.parametrize(
+		'item_ids_length', [number for number in range(2, 11, 1)])
+	def test_item_ids_different_length_positive(
+			self, user_client_id, user_address, user_phone, item_ids_length):
+		"""
+		Проверка позитивных сценариев для введенного количества нескольких
+		`item_id` при создании заказа.
+		"""
+
+		_data = {
+			'client_id': user_client_id,
+			'address': user_address,
+			'phone': user_phone,
+			'items': []
+		}
+
+		for item_id in range(item_ids_length):
+			_data['items'].append({})
+			_data['items'][item_id].update({
+					'item_id': random.randint(10000, 99999),
+					'price': float(random.randint(100, 1000)),
+					'quantity': random.randint(1, 20)
+				}
+			)
+
+		items = [item['item_id'] for number, item in enumerate(_data['items'])]
+		count = dict(
+			(x, items.count(x))
+			for x in set(items)
+			if items.count(x) > 1
+		)
+
+		if not count:
+			assert True, 'Different item_ids'
+		elif len(count.values()) > 1:
+			assert False, 'Same item_ids'
+		elif list(count.values())[0] > 1:
+			assert False, 'Same item_ids'
+
+	@pytest.mark.parametrize(
 		'param_order_item_id', test_data.check_neg_item_ids())
 	def test_item_id_negative(
 			self, param_order_item_id, create_order_url, user_client_id,
 			user_address, user_phone, order_price, order_quantity):
 		"""
-		Проверка негативных сценариев для введеного количества `item_id`
+		Проверка негативных сценариев для введенного количества `item_id`
 		при создании заказа.
 		"""
 
@@ -77,3 +118,43 @@ class TestOrderItemId:
 			assert True, f'HTTP Error: {err}'
 		except requests.exceptions.ConnectionError:
 			assert False, 'ConnectionError'
+
+	@pytest.mark.parametrize(
+		'item_ids_length', [number for number in range(2, 10, 2)])
+	def test_item_ids_different_length_negative(
+			self, user_client_id, order_item_id, user_address, user_phone,
+			item_ids_length):
+		"""
+		Проверка негативных сценариев для введенного количества нескольких
+		`item_id` при создании заказа.
+		"""
+
+		_data = {
+			'client_id': user_client_id,
+			'address': user_address,
+			'phone': user_phone,
+			'items': []
+		}
+
+		for item_id in range(item_ids_length):
+			_data['items'].append({})
+			_data['items'][item_id].update({
+					'item_id': order_item_id,
+					'price': float(random.randint(100, 1000)),
+					'quantity': random.randint(1, 20)
+				}
+			)
+
+		items = [item['item_id'] for number, item in enumerate(_data['items'])]
+		count = dict(
+			(x, items.count(x))
+			for x in set(items)
+			if items.count(x) > 1
+		)
+
+		if len(count) > 1:
+			assert True, 'Same item_ids'
+		elif list(count.values())[0] > 1:
+			assert True, 'Same item_ids'
+		elif not count:
+			assert False, 'Different item_ids'
